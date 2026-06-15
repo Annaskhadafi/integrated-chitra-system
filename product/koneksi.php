43446ce1761208927cb1c4be7e793356ffff04bd
LuthfiSay <luthfisaiful27@gmail.com>
2026-06-15 16:07:33 +0800

    perbaikan koneksi.php

--- product/koneksi.php
+++ product/koneksi.php
@@ -1,9 +1,9 @@
 <?php
 // Menggunakan getenv() untuk mengambil konfigurasi dari environment variables Dokploy
 // Jika tidak ada, maka akan menggunakan default (untuk fallback)
-$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_host = getenv('DB_HOST') ?: 'integrated-chitra-system-database-x77gvi';
 $db_user = getenv('DB_USER') ?: 'root';
-$db_pass = getenv('DB_PASS') ?: '';
+$db_pass = getenv('DB_PASS') ?: 'userdatabaseics27693';
 
 $db_name  = getenv('DB_NAME') ?: "chitraparatama_ics";
 $db_name2 = getenv('DB_NAME2') ?: "chitraparatama_fleetlist";
