f2c640000114f1f5e90ff35805992c57c521c683
LuthfiSay <luthfisaiful27@gmail.com>
2026-06-15 16:04:54 +0800

    tambahkan source

--- product/halamanrmiprice.php
+++ product/halamanrmiprice.php
@@ -1,5 +1,4 @@
 <?php
-session_start();
 include "koneksi.php";
 
 // --- LOGIKA FILTER TAHUN ---
@@ -27,9 +26,13 @@ if ($selectedYear != "") {
 
 $query = "SELECT * FROM material_price $whereClause ORDER BY price_date ASC, material_name ASC";
 $result = mysqli_query($koneksi, $query);
+
+$query2 = "SELECT DISTINCT(material_name) as material ,source FROM `material_price` ORDER BY `material_price`.`source` DESC;";
+$result2 = mysqli_query($koneksi, $query2);
 // --- END LOGIKA FILTER ---
 
 $dataHasil = [];
+$dataSorce = [];
 $labels = [];
 $temp_data_mineral = [];
 $temp_data_material = [];
@@ -234,7 +237,41 @@ $datasetsMaterial = buildDataset($temp_data_material, $labels, $material_info, $
                   </table>
               </div>
           </div>
-
+          <div class="x_panel">
+              <div class="x_title">
+                  <h2>Data Source</h2>
+                  <div class="clearfix"></div>
+              </div>
+              <div class="table-responsive">
+                  <?php
+                  $querySource = "SELECT DISTINCT material_name as material, source FROM material_price ORDER BY source DESC";
+                  $resSource = mysqli_query($koneksi, $querySource);
+                  
+                  if (!$resSource) {
+                      echo "Error: " . mysqli_error($koneksi);
+                  } elseif (mysqli_num_rows($resSource) == 0) {
+                      echo "<p>Tidak ada data sumber tersedia.</p>";
+                  } else {
+                  ?>
+                  <table class="table table-striped table-bordered">
+                    <thead>
+                        <tr style="background:#f5f5f5;">
+                            <th>Material Name</th>
+                            <th>Source</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <?php while ($rowSrc = mysqli_fetch_assoc($resSource)): ?>
+                            <tr>
+                                <td><?= htmlspecialchars($rowSrc['material']) ?></td>
+                                <td><?= htmlspecialchars($rowSrc['source'] ?: '-') ?></td>
+                            </tr>
+                        <?php endwhile; ?>
+                    </tbody>
+                  </table>
+                  <?php } ?>
+              </div>
+          </div>
         </div>
       </div>
     </div>
@@ -243,6 +280,14 @@ $datasetsMaterial = buildDataset($temp_data_material, $labels, $material_info, $
     <script src="../vendors/bootstrap/dist/js/bootstrap.min.js"></script>
     <script src="../vendors/datatables.net/js/jquery.dataTables.min.js"></script>
     <script src="../vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+    
+    <!-- DataTables Buttons -->
+    <script src="../vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+    <script src="../vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
+    <script src="../vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
+    <script src="../vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
+    <script src="../vendors/jszip/dist/jszip.min.js"></script>
+
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
     <script>
@@ -292,6 +337,9 @@ $datasetsMaterial = buildDataset($temp_data_material, $labels, $material_info, $
         if ($("#datatable-buttons").length) {
           $("#datatable-buttons").DataTable({
             dom: "Bfrtip",
+            buttons: [
+                { extend: "excelHtml5", text: "Export Excel", className: "btn-sm btn-primary" }
+            ],
             order: [[ 6, "desc" ]]
           });
         }
